55f9ad5c173bd5a849a93bfa33ec7c8ada22b4f4
kaicui <wbpmrck2003@gmail.com>
2017-06-23 11:34:32 +0800

    修改描述。修改validation库，增加cmd loader模式 以及 无loader情况下的demo

--- demo/demoData/OneLib.Validation/example.1.js
+++ demo/demoData/OneLib.Validation/example.1.js
@@ -1,33 +1,31 @@
-define('testValidate1', ['OneLib.Validation'], function (require, exports, module) {
-    var validation = require('OneLib.Validation'),wnd = window;
-    
-    //添加一个自定义验证器，验证某个输入必须在指定范围内
-    validation.setValidateFunction("mustBetween", {
-        /**
-         * 验证函数体，接收from,to 2个参数，让用户指定比较范围
-         */
-        fn:function (from,to, cb) {
-            //验证器逻辑：当被验证的值(origin)在from,to之间时，为true(通过)
-            cb&&cb(this.origin >= from && this.origin <= to,"输入必须在[{1},{2}]之间");
-        },
-        desc:'输入必须在[{1},{2}]之间' //如果fn中的cb没有给出第二个参数，会默认用这个描述来返回
-    });
-    var valid = validation.targetWrapper;//方便使用
-    
-    exports.runTest = function(){
-        var userInput1 =12; //模拟用户输入
-        var result = valid(userInput1,"用户输入的内容1").mustBetween(11,21).run();
-        result.then(function (ret) {
-            wnd.alert('ret is:'+JSON.stringify(ret));
-        });
-        
-    }
-    exports.runTest2 = function(){
-        var userInput1 =123; //模拟用户输入
-        var result = valid(userInput1,"用户输入的内容1").mustBetween(11,21).run();
-        result.then(function (ret) {
-            wnd.alert('ret is:'+JSON.stringify(ret));
-        });
-        
-    }
+var validation = OneLib.Validation,wnd = window;
+
+//添加一个自定义验证器，验证某个输入必须在指定范围内
+validation.setValidateFunction("mustBetween", {
+    /**
+     * 验证函数体，接收from,to 2个参数，让用户指定比较范围
+     */
+    fn:function (from,to, cb) {
+        //验证器逻辑：当被验证的值(origin)在from,to之间时，为true(通过)
+        cb&&cb(this.origin >= from && this.origin <= to,"输入必须在[{1},{2}]之间");
+    },
+    desc:'输入必须在[{1},{2}]之间' //如果fn中的cb没有给出第二个参数，会默认用这个描述来返回
 });
+var valid = validation.targetWrapper;//方便使用
+
+var test1 = function(){
+    var userInput1 =12; //模拟用户输入
+    var result = valid(userInput1,"用户输入的内容1").mustBetween(11,21).run();
+    result.then(function (ret) {
+        wnd.alert('ret is:'+JSON.stringify(ret));
+    });
+    
+}
+var test2 = function(){
+    var userInput1 =123; //模拟用户输入
+    var result = valid(userInput1,"用户输入的内容1").mustBetween(11,21).run();
+    result.then(function (ret) {
+        wnd.alert('ret is:'+JSON.stringify(ret));
+    });
+    
+}
