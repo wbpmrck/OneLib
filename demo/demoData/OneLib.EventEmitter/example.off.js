301585dae9f1c220427a5df97cc78217cc1a3812
kaicui <wbpmrck2003@gmail.com>
2017-08-22 11:01:15 +0800

    更新demo中的笔误

--- demo/demoData/OneLib.EventEmitter/example.off.js
+++ demo/demoData/OneLib.EventEmitter/example.off.js
@@ -4,4 +4,4 @@ aDog.off('shouted',callback);//把shouted的处理函数callback移除
 //或者使用on返回的SubscribeId
 var SubscribeId = aDog.on("shout",function(){});
 
-aDog.off(SubscribeId);//这样也可以
+aDog.off("shout",SubscribeId);//这样也可以
